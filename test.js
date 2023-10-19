47f1db06d4a78e2de4dc8586250dd4dfff306ab2
Mojithmj <mojithmj@gmail.com>
2023-10-19 09:54:18 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+let a=-5,b=-2,c=-6,d=6,e=-1;
+if(a>b&&a>c&&a>d&&a>e)
+{
+    console.log("a is greater");
+}
+else if(b>a&&b>c&&b>d&&b>e)
+{
+    console.log("b is greater");
+}
+else if(c>a&&c>b&&c>d&&c>e)
+{
+    console.log("c is greater");
+}
+else if(d>a&&d>b&&d>c&&d>e)
+{
+    console.log("d is greater");
+}
+else
+{
+    console.log("e is greater");
+}
